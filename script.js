ccb539872f9ceeb2814c0deba6c040a577a165cd
ar-kenzz <makdefin2005@gmail.com>
2025-09-26 03:10:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // ===== Daftar dokumen =====
 const docs = [
   { title: "Revisi", link: "https://ar-kenzz.github.io/Thoughts/Doc1/" },
+  { title: "Revisi", link: "https://ar-kenzz.github.io/Thoughts/Doc1/" },
   { title: "Menuju Indonesia Emas", link: "https://ar-kenzz.github.io/Thoughts/Doc2/" }
 ];
 
@@ -84,3 +85,4 @@ const modalClose=document.getElementById("modalClose");
 
 modalClose.addEventListener("click",()=>modal.classList.remove("show"));
 modal.addEventListener("click",e=>{if(e.target===modal) modal.classList.remove("show");});
+
